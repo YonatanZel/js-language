9271f54fc854b822d556474db8e5809fd2d5a57d
YonatanZel <aartemiyev@gmail.com>
2022-01-15 22:59:19 +0200

    commit

--- app.js
+++ app.js
@@ -1,68 +1,53 @@
-//console.log("hello world");
-//output for var 3 3 3
-// for(var i = 0; i < 3; i++){
-//     setTimeout(function() {
-//         console.log(i);
-//     })
-// }
+/************************Strings */
+// const str1 = "abc";
+// const str2 = "abc";
+// console.log(`srt1 == str2 is ${str1 == str2}`);
+// console.log(`srt1 < s is ${str1 < "s"}`);
+// console.log(`"123" > 23 is ${"123" > 23}`);
+// /*********************************************** */
+// console.log(`"123" > 23 is ${"123" > 23}`);
+// console.log(`"abc" < 23 is ${"abc" < 23}`);
+// console.log(`"abc" != 23 is ${!("abc" == 23)}`);
+// /************************************************* */
+function stringProcessing(str){
+    const strP = "" + str;
+    console.log(`"${str}"[4] is ${str[4]}`);
+    console.log(`length of "${str}" is ${strP.length}`);
+    console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
+    console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2,5)}`);
+    console.log(`index of substring "ll" in the "${str}" is; last index of substring l in the "${str}" is ${strP.indexOf("ll")}; ${strP.lastIndexOf("ll")}`);
 
-// for(let i = 0; i < 3; i++){
-//     setTimeout(function() {
-//         console.log(i);
-//     })
-// }
-// const fun = function(a,b){
-//     return a + b;
-// }
-//let a = 5;
-//console.log(a ** 2);
-//console.log(fun(10,20));
-//console.log(a(10,20)); error
-//console.log(fun ** 2); NaN
-// console.log("12" + 12, '"12" + 12');
-// console.log("12" - 12);
-// console.log("ab" - 12);
-// console.log(+"12" + 12);
-// console.log("12" + 12, '"12" + 12');
-// console.log(`+"12" + 12 = ${+"12" + 12}`);
-
-
-
-//homework 10 definition
-//1. using only two letters "a", "s" print out word ananas case insensitive AnaNas.
-//2. write function calculate that can perform any arithmetic 
-//operation on two numbers(*,/,+,-).
-//3. write any example for running the following expression fun(5)(10,3)
-
-//1.
-const fun = function(a,b){
-    return a + b;
 }
-console.log("a" + fun**2 + "a" + "s");
+stringProcessing("abcd*lmn*ll(oo&&tt");
 
-//2.
-const calc = function(a,b,action){
-    let res=-1;
-switch(action)
-{
-    case 1: res=a+b;
-    break;
-    case 2: res=a-b;
-    break;
-    case 3: res=a*b;
-    break;
-    case 4: 
-    if(b!=0){
-        res=a/b;
-    }else console.log("cant division by zero");
-    break;
-    default: console.log("wrong action");
+function encode(num, base) {
+    //base from 2 to 10
+    let res = "";
+    do {
+         const digit = Math.trunc(num % base);
+         const symb = getSymbol(digit);
+         res = symb + res;
+         num = Math.trunc(num / base);
+    } while(num >= 1);
+    return res;
 }
-return res;
+function getSymbol(digit){
+    console.log(digit);
+    return "" + digit; //it will work only for base <=10
 }
-console.log(calc(2,3,1));
+console.log(encode(10,2));
 
-
-
+function encode2(num){
+    let res = "";
+    do{
+        const btoa = Math.random().toString(36).substring(3, 5);
+        const digit = Math.trunc(num % 10);
+        digit == btoa; 
+        res = btoa + res;
+        num = Math.trunc(num / 10);
+        }while(num>=1);
+    return res;
+}
+console.log(encode2(15879));
 
 
